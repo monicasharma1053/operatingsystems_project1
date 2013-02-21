fe6769810ff88738ee83ef1d6647d019ec04368e
Monica Sharma <monicasharma1053@gmail.com>
2013-02-20 22:06:26 -0600

    definitions and printing tree function

--- hw1.c
+++ hw1.c
@@ -20,3 +20,35 @@ struct node {
     struct node *child;
     struct node *sibling;
 };
+
+int customLine(char *line, FILE *ps_pipe, int size);
+int growTree(struct node *node, int pid, int ppid);
+struct node *findNode(struct node *node, int pid);
+void printTree(struct node *tree, int indentSize);
+
+
+//print tree with nodes 
+void printTree(struct node *node, int indentSize)
+{
+
+    //set indents 
+    char indents[128];
+    //tab per line 
+    char *indentLine = "\t";
+    //set memory with space for indents 
+    memset(indents, 0, sizeof(indents));
+
+    int i; 
+    for (i = 0; i < indentSize; i++)
+    {
+        strcat(indents, indentLine);
+    }
+
+    printf("%s|->%d\n",indents, node->pid);
+    //increase indent for children 
+    if (node->child) printTree(node->child, indentSize+1);
+
+    //decrease indent for siblings
+    if (node->sibling) printTree(node->sibling, indentSize);
+}
+
